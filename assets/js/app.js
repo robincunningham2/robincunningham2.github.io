a055c72d4ee8f2e7020dbbf390453218126256a3
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2021-01-09 19:30:37 +0100

    Update: data loading system

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,22 @@ const resizeCallback = () => {
 
 $(window).resize(resizeCallback);
 resizeCallback();
+
+const dataCallback = (data) => {
+    $('div.grid-container').html(null);
+
+    data.portfolio.forEach(proj => {
+        const element = `
+        <a href="${proj.repo}" target="_blank">
+            <div class="item">
+                <div class="img" style="background-image: url(${proj.image});"></div>
+                <div class="text">${proj.name}</div>
+            </div>
+        </a>
+        `;
+
+        $('div.grid-container').append(element);
+    });
+};
+
+$.get('data.json', dataCallback);
